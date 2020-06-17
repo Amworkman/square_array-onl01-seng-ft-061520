cd74bb33bf7aa2a7691de60c7fb46dfc5792aa61
Adam Workman <adammworkman25@gmail.com>
2020-06-17 14:20:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   new_numbers_array []
-  square_array.each do |numbers| << 
-    numbers ** 2 
+  square_array.each do |numbers| << numbers ** 2
+    new_numbers_array
 end
 end
